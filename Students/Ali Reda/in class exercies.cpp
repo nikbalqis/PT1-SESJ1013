2885cc2ef54e6754251430d1042bf875b4a899dc
ali reda <147963452+aliredaaliabouseada@users.noreply.github.com>
2023-10-22 13:14:47 +0800

    2

---
+++ Students/Ali Reda/in class exercies.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  int number;
+  double decimal;
+  char letter;
+  char name [21];
+  cout<< "ENTER AN INTEGER" << endl;
+  cin>> number;
+  cout<< "ENTER A DECIMAL NUMBER" << endl;
+  cin>> decimal;
+  cout<< "ENTER A SINGLE CHARACTER" << endl;
+  cin>> letter;
+  cout<< "ENTER AA ARRY OF CHARACTER"<< endl;
+  cin>> name;
+  return 0;
+
+}
