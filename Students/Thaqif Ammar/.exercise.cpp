87da4114d702c0e75b9880fc2834c55aa3b3ad6a
thaqifammmar89 <thaqifa89@gmail.com>
2023-10-22 21:55:03 +0800

    exercie

---
+++ Students/Thaqif Ammar/.exercise.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int x;
+    double y;
+    char typess;
+
+    cout<< "Enter an integer: ";
+    cin>> x;
+    cout<< "ENter a decimal number: ";
+    cin>> y;
+    cout<< "Enter a single character: ";
+    cin>> typess;
+
+    system("pause");
+    return 0;
+    }
