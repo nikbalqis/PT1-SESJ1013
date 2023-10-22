03d476120b7dacfe156ff6f8c132a6806f315d4d
Vibhushas <147962293+Vibhushas@users.noreply.github.com>
2023-10-22 12:48:41 +0800

    Update inClassExercise.cpp

---
+++ Students/Vibhusha/inClassExercise.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int num;
+    double num1;
+    char alphabet, alphabets [21];
+
+    cout<< "Enter an integer:";
+    cin >> num;
+    cout<< "Enter a decimal number:";
+    cin >> num1;
+    cout<< " Enter a single character:";
+    cin >> alphabet;
+    cout<< "Enter an array of characters:";
+    cin >> alphabets;
+
+
+    return 0;
+}
