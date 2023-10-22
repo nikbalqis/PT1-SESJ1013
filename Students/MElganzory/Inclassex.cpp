35845df498554b785ae88cc668b2094fed012297
MElganzory <148404925+MElganzory@users.noreply.github.com>
2023-10-22 12:19:57 +0800

    Update Inclassex.cpp

--- Students/MElganzory/Inclassex.cpp
+++ Students/MElganzory/Inclassex.cpp
@@ -4,18 +4,18 @@ using namespace std;
 
 int main()
 {
-    int num1;
-    double num2;
-    char let;
-    char word[21];
+    int num1; // defines an integer
+    double num2; // defines a a decimal number
+    char let; // defines a letter
+    char word[21]; // defines a word
 
-    cout << "Enter an integer:" << endl;
-    cin >> num1;
-    cout << "Enter a decimal number:" << endl;
-    cin >> num2;
-    cout << "Enter a single character:" << endl;
-    cin >> let;
-    cout << "Enter an array of characters:" << endl;
-    cin >> word;
+    cout << "Enter an integer:" << endl;  // outputs enter an integer for the user
+    cin >> num1; // Asks the user to input an integer
+    cout << "Enter a decimal number:" << endl; // outputs Enter a decimal number for the user
+    cin >> num2; // Asks the user to input a decimal number
+    cout << "Enter a single character:" << endl; // outputs Enter a single character for the user
+    cin >> let; // Asks the user to input a single character
+    cout << "Enter an array of characters:" << endl; // outputs Enter an array of characters for the user
+    cin >> word; // Asks the user to input an array of characters
     return 0;
 }
