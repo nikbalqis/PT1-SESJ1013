ca38fa12400ccc859ea2627c131743099d2d6992
VishaKim16 <147959793+VishaKim16@users.noreply.github.com>
2023-10-22 12:42:57 +0800

    Create Inclassactivity1.cpp

---
+++ Students/Vishali/Inclassactivity1.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    int integer;
+    double decimal;
+    char character;
+    char word;
+
+    cout <<"Enter an integer:";
+    cin >> integer;
+
+    cout <<"Enter a decimal number:";
+    cin >> decimal;
+
+    cout <<"Enter a single character:";
+    cin >> character;
+
+    cout <<"Enter an array of characters:";
+    cin >> word;
+
+
+    return 0;
+}
