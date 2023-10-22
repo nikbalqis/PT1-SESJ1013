7b61f66b407d2b60cf60215d1d1e5507c3e14487
aniqy05 <aniqaziq17@gmail.com>
2023-10-22 12:40:03 +0800

    In Class Exercise

---
+++ Students/AniqAziq/exercise1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num;
+    float decimal;
+    char single[1],word[20];
+    
+    cout<<"Enter an integer : ";
+    cin>>num;
+    cout<<"Enter a decimal number : ";
+    cin>>decimal;
+    cout<<"Enter a single character : ";
+    cin>> single;
+    cout<<"Enter an array of characters : ";
+    cin>>word;
+
+    system("pause");
+    return 0;
+}
