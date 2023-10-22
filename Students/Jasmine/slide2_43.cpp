de0b0b6285f33b46ab6882f017c7e26675ff4a48
tjasmiine <147676875+tjasmiine@users.noreply.github.com>
2023-10-22 12:55:53 +0800

    1

--- Students/Jasmine/slide2_43.cpp
+++ Students/Jasmine/slide2_43.cpp
@@ -12,7 +12,7 @@ int main (){
     cin >> decinum;
     cout << "Enter a single character : ";
     cin >> scharacter;
-    cout << "Enter an array of characters : ";
+    cout << "Enter an array of characters llll : ";
     cin >> acharacter;
 
     return 0;
