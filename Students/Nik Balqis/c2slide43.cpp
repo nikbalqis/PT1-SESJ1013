cc9185dde5d00755b5050b931ea5a79fce43b595
nikbalqis <148343355+nikbalqis@users.noreply.github.com>
2023-10-23 22:02:06 +0800

    Add files via upload

---
+++ Students/Nik Balqis/c2slide43.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int int_value;
+    double dec_value;
+    char single_character, character_array[100];
+
+    cout << "Enter an integer: ";
+    cin >> int_value;
+    cout << "Enter a decimal number: ";
+    cin >> dec_value;
+    cout << "Enter a single character: ";
+    cin >> single_character;
+    cout << "Enter an array of characters: ";
+    cin >> character_array;
+
+    return 0;
+}
