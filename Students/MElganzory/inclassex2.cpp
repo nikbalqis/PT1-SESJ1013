5d7813a5dad0a9e7bcb40e094a6d5fda9d81d029
Mahmoud Mustafa Elganzory <148404925+MElganzory@users.noreply.github.com>
2023-10-26 09:26:41 +0800

    Create inclassex2.cpp

---
+++ Students/MElganzory/inclassex2.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+double a = 46/9;
+double b = 46 % 9 + 4 * 4 - 2;
+double c = 45 + 43 % 5 * (23 * 3 % 2);
+int d = d % 3 / a + 3;
+double e = e + 1.5 * 3 + (++a);
+double f = f - 1.5 * 3 + a++;
+    cout << "The value of A is" << a <<endl;
+    cout << "The value of B is" << b <<endl;
+    cout << "The value of C is" << c <<endl;
+    cout << "The value of D is" << d <<endl;
+    cout << "The value of E is" << e <<endl;
+    cout << "The value of F is" << f <<endl;
+
+    return 0;
+}
