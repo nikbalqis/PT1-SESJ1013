d67fcc50314e84e5c7856a7d1881390e0f30e763
Mahmoud Mustafa Elganzory <148404925+MElganzory@users.noreply.github.com>
2023-10-26 09:50:06 +0800

    Update inclassex2.cpp

--- Students/MElganzory/inclassex2.cpp
+++ Students/MElganzory/inclassex2.cpp
@@ -8,14 +8,18 @@ int a = 1;
 double d = 1.0;
 
 a = 46/9;
-a = 46 % 9 + 4 * 4 - 2;
-a = 45 + 43 % 5 * (23 * 3 % 2);
-a %=3 / a + 3;
-d += 1.5 * 3 + (++a);
-d -= 1.5 * 3 + a++;
-
 cout << "The value of A =" <<  a << endl;
+a = 46 % 9 + 4 * 4 - 2;
+cout << "The value of A =" <<  a << endl;
+a = 45 + 43 % 5 * (23 * 3 % 2);
+cout << "The value of A =" <<  a << endl;
+a %=3 / a + 3;
+cout << "The value of A =" <<  a << endl;
+d += 1.5 * 3 + (++a);
 cout << "The value of D =" <<  d << endl;
+d -= 1.5 * 3 + a++;
+cout << "The value of D =" <<  d << endl;
+
 
     return 0;
 }
