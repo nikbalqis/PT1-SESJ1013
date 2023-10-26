9bc5118b270a086dc714933c0c243f009ba2cb86
thaqifammmar89 <thaqifa89@gmail.com>
2023-10-26 09:42:09 +0800

    inclassExercise

---
+++ Students/Thaqif Ammar/inclassExercise.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a=1;
+    double d=1.0;
+
+    a=46/9;
+    cout<<a<< endl;
+
+    a = 46 % 9 + 4 * 4 -2;
+    cout<<a<<endl;
+
+    a=45 + 43 % 5 * (23 * 3 % 2);
+    cout<<a<<endl;
+
+    a%=3 / a + 3;
+    cout<<a<<endl;
+
+    d += 1.5 * 3 + (++a);
+    cout<<d<<endl;
+
+    d -= 1.5 * 3 + a++;
+    cout<<d<<endl;
+
+
+
+    system("pause");
+    return 0;
+}
