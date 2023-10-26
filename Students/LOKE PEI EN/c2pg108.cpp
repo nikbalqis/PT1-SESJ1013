c609292cf75e3b649afd78d53def49f6ee5a0594
lokepeien <147963372+lokepeien@users.noreply.github.com>
2023-10-26 10:16:44 +0800

    1

---
+++ Students/LOKE PEI EN/c2pg108.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a=1;double d=1.0;
+
+    a= 46/9;
+
+    a=46%9+4*4-2;
+
+    a=45+43%5*(23*3%2);
+
+    a%=3/a+3;
+
+    d+=1.5*3+(++a);
+
+    d-=1.5*3+a++;
+
+    cout<<"a"<<endl;
+
+    cout<<"d"<<endl;
+
+    return 0;
+
+
+
+}
