b0f30b36b64cd57ce2cedc99f3d90878d1e97c06
ZULQARNAIN04 <zulqarnaincyrus@gmail.com>
2023-10-26 09:59:39 +0800

    exercise 2

--- Students/Zulqarnain/exercise2.cpp
+++ Students/Zulqarnain/exercise2.cpp
@@ -3,22 +3,53 @@
 int main(){
     int a=1;
     double d=1.0;
+    int i=0;
 
-    a = 46/9;//question 1
-    std::cout<<"\na is "<<a << " d is "<<d;
+    while(i<6){
 
-    a = 46 % 9 + 4 * 4 - 2;//2
-    std::cout<<"\na is "<<a << " d is "<<d;
+        switch(i){
+            case 0:
+                a = 46/9;//question 1
+                std::cout<<"\na is "<<a << " d is "<<d;
+                break;
+            case 1:
+                a = 46 % 9 + 4 * 4 - 2;//2
+                std::cout<<"\na is "<<a << " d is "<<d;
+                break;
+            case 2:
+                a = 45 + 43 % 5 * (23 * 3 % 2);//3
+                std::cout<<"\na is "<<a << " d is "<<d;
+                break;
+            case 3:
+                a %=3 / a + 3;//4
+                std::cout<<"\na is "<<a << " d is "<<d;
+                break;
+            case 4:
+                d += 1.5 * 3 + (++a);//5
+                std::cout<<"\na is "<<a << " d is "<<d;
+                break;
+            default:
+                d -= 1.5 * 3 + a++;//6
+                std::cout<<"\na is "<<a << " d is "<<d;
 
-    a = 45 + 43 % 5 * (23 * 3 % 2);//3
-    std::cout<<"\na is "<<a << " d is "<<d;
+        }
+        a=1;
+        d=1.0;
+        i++;
 
-    a %=3 / a + 3;//4
-    std::cout<<"\na is "<<a << " d is "<<d;
+    }
 
-    d += 1.5 * 3 + (++a);//5
-    std::cout<<"\na is "<<a << " d is "<<d;
+    
 
-    d -= 1.5 * 3 + a++;//6
-    std::cout<<"\na is "<<a << " d is "<<d;
+    
+
+    
+
+    
+
+    
+
+    
+
+    
 }
